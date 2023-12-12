7589bb47b6c8d30c61311798e32f843f13146083
gabriel <dgabrielfof@gmail.com>
2023-12-12 18:17:59 -0300

    :card_file_box: raw: adding data.js with personal information

---
+++ src/data.js
@@ -0,0 +1,486 @@
+export const data = {
+  nome: "Danilo Gabriel Fagundes de Oliveira Ferreira",
+  education: [
+    {
+      title: "Bacharel em Engenharia Química",
+      providedBy: "Universidade Federal da Bahia",
+      concludedIn: new Date(2021, 6, 25),
+    }
+  ],
+  skills: [
+    {
+      name: "Desenvolvimento de Aplicativos",
+      origin: "Durante a minha adolecência comecei a ser fâ de jogos e isso me despertou um enorme interesse em como eles eram construídos e o poder que tinham de contar histórias, o meu primeiro contato com a programação foi realizando um servidor modificado de um jogo. Mais tarde eu pude desenvolver aplicatições que me ajudaram a realizar minhas tarefas com excelência.",
+      achievements: [
+        "Análise crítica dos cenários",
+        "Criatividade para criar solução",
+        "Um porquê como combustível",
+      ]
+    },
+    {
+      name: "Automação de processos",
+      origin: "Na minha primeira experiência proficional, após a formatura, eu me tornei responsável pela parte administrativa de materiais de manutenção hospitalar. Só que ficar preenchendo planilhas e conferindo as notas uma por uma para saber em que situação estão. Resolvi fazer diferente, gastar meu tempo pra deixar algo ao invés de só manter as coisas.",
+      achievements: [
+        "Aplicar uma paixão (programação) para resolver um problema",
+        "Realizar automações de rotinas administrativas com Python",
+        "Facilitar a minha vida e a vida dos meus colegas",
+      ]
+    },
+    {
+      name: "Melhoria Contínua",
+      origin: "Durante a faculdade, cursei uma disciplina que falava sobre sustentabilidade, sobre os recursos e sobre os planetas. Ela parecia distoar de todas as outras disciplinas do curso, e todos pensavam isso, creio eu. Mas enquanto todos analizavam financeiramente os projetos, eu comecei a me questionar sobre a economia gerada pela sustentabilidade. E enquanto buscava um emprego após a faculdade fiz alguns cursos voltados ao Lean e Six Sigma, que corroboraram através de uma visão mais pé no chão com a ideia de otimizar os processos",
+      achievements: [
+        "Cada projeto que desenvolvi teve como base a redução de desperdícios",
+        "Enxergar os obstáculos como o caminho para o crescimento",
+        "Resultados como argumentos",
+      ]
+    },
+    {
+      name: "Gestão de projetos",
+      origin: "Na minha primeira experiência, quando fiquei encarregado de uma tarefa rotineira, eu tive a necessidade de fazer aquilo de outra forma, meu superior direto me disse: Faz o seu trabalho do melhor jeito que puder. E isso foi maravilhoso, inclusive agradeço a ele pela grande gestão e espaço de crescimento. Emtão me juntei com os estagiários e elaboramos um plano de ação, prazos, requisitos necessários e foi maravilhoso entregar meu primeiro projeto e ser indicado ao prêmio de inovação da empresa",
+      achievements: [
+        "Desenho de projeto",
+        "Trabalho em equipe",
+        "Divisão de tarefas",
+        "Apresentação de resultados",
+        "Melhoria de projetos",
+      ]
+    },
+    {
+      name: "Design de Processos Industriais",
+      origin: "Durante meu estágio, a minha atividade principal era estudar o processo de separação da mistura de óleo e água. Então estudei cada etapa do processo, o que chegava, como chegava, quem enviava, o que acontecia, o que saía e pra onde ia. Isso me fez conhecer e estudar muitos equipamentos envolvidos no processo de separação para que a empresa pudesse definir as especificações de cada equipamento de acordo com suas dimensões",
+      achievements: [
+        "Fluxos de processos",
+        "Equipamentos de separação e tratamento de efluentes",
+        "Análise de mercado",
+      ]
+    },
+    {
+      name: "Pacote Office",
+      origin: "Desde o estágio, onde fiz a transição do fluxo de caixa para o meio digital e criei os primeiros documentos padronizados dos equipamentos. Venho aprendendo mais ferramentas que auxiliam no dia a dia profissional e pessoal a cada necessidade que surge.",
+      achievements: [
+        "Estruturação de bancos de dados",
+        "Tabelas dinâmicas",
+        "Dashboards",
+        "Macros",
+        "Apresentação",
+        "Relatórios",
+        "Libre Office",
+        "Microsoft Office",
+      ]
+    }
+  ],
+  languages: [
+    {name: "português", level: "nativo"},
+    {name: "inglês", level: "A2"},
+  ],
+  contacts: [
+    {name: "Telefone", number: "(71) 99321-0108"},
+    {name: "GitHub", link: "http://github.com/DGabrielF", text: "/DGabrielF"},
+    {name: "Linkedin", link: "https://www.linkedin.com/in/danilo-ferreira-092457127", text: "/danilo-ferreira-092457127"},
+    {name: "Facebook", link: "https://www.facebook.com/danilo.ferreira.186590", text: "/danilo.ferreira.186590"},
+  ],
+  experiences: [
+    {
+      position: "Trainee em Pesquisa e Desenvolvimento",
+      company: "Liga Álvaro Bahia Contra a Mortalidade Infantil",
+      activities: [
+        "Responsável pela gestão de materiais de manutenção hospitalar (água, energia, gases medicinais, diesel) e estudo de viabilidade para projetos de eficiência no consumo.",
+        "Desenvolvimento de softwares para acompanhamento de notas, estoque e medidores, geração de relatórios automáticos."
+      ],
+      developedProjects: [
+        {
+          name: "NIX",
+          goals: [
+            "Reduzir o tempo utilizado para atualizar planilhas",
+            "Reduzir erros de digitação",
+            "Evitar que notas fiscais ficassem sem registro",
+            "Facilitar a localização de notas",
+            "Auxiliar na visualização do estado das notas.",
+          ],
+          toolsUsed: [
+            "Voz do Cliente",
+            "Análise de falhas",
+            "Gerenciamento de banco de dados",
+            "Ferramentas de priorização",
+            "Desenho do projeto",
+            "Python",
+            "Numpy",
+            "Pandas",
+            "Tkinter",
+            "Smtplib",
+            "Firebase",
+            "Documentação da aplicação"
+          ]
+        },
+        {
+          name: "COCITE",
+          goals: [
+            "Elevar a credibilidade do controle de temperatura da empresa em vista do órgão fiscalizador",
+            "Requisitar periódicamente os dados de um banco de dados em tempo real de monitoramento",
+            "Diagnósticar o comportamento dos refrigeradores de acordo com os intervalos de temperatura permitidas",
+            "Geração de um relatório com dados estatísticos, diagnóstico de comportamento e atitudes a serem tomadas, caso necessário.",
+          ],
+          toolsUsed: [
+            "Consumo de dados via API",
+            "Padronização de documentos",
+            "Análise estatística",
+            "Elaboração de indicadores",
+            "Python",
+            "Numpy",
+            "Pandas",
+            "Matplotlib",
+          ]
+        },
+        {
+          name: "TÁCITA",
+          goals: [
+            "Mapear o sistema de monitoramento de ruído para replicação",
+            "Replicar o dispositivo"
+          ],
+          toolsUsed: [
+            "Componentes eletrônicos",
+            "Conexões eletrônicas",
+            "Desenho técnico",
+          ]
+        },
+        {
+          name: "Treinamento em SolidWorks",
+          goals: [
+            "Desenvolver modelos de peças de equipamentos para a impressora 3D"
+          ],
+          toolsUsed: [
+            "Desenho técnico",
+            "Modelagem 3D",
+          ]
+        }
+      ]
+    },
+    {
+      position: "Estagiário de Processos",
+      company: "Fluxotécnica Equipamentos Industriais LTDA",
+      activities: [
+        "Migração de documentos para o meio digital (fluxo de caixa, fichas técnicas e procedimentos operacionais).",
+        "Documentação das fichas técnicas dos equipamentos.",
+        "Dimensionamento e pesquisa de mercado para equipamentos de separação.",
+        "Mapeamento de fluxo dos processos de separação e de tratamento de efluentes."
+      ],
+      developedProjects: [
+        {
+          name: "Fluxo de caixa",
+          goals: [
+            "Estruturar o banco de dados",
+            "Transcrever os dados físicos para o meio digital"
+          ],
+          toolsUsed: [
+            "Gerenciamento de banco de dados",
+            "Microsoft Excel",
+          ]
+        },
+        {
+          name: "Extração de óleo onshore",
+          goals: [
+            "Conhecer os equipamentos",
+            "Conhecer a sequência de separação",
+            "Definir as especificações de um separador",
+          ],
+          toolsUsed: [
+            "Plataformas de comércio eletrônicos",
+            "Microsoft Excel",
+            "Microsoft Word",
+          ]
+        }
+      ]
+    }
+  ],
+  certifications: [
+    {
+      name: "Programação em Linguagem Python",
+      providedBy: "Universidade Federal da Bahia",
+      concludedIn: new Date(2022, 9, 20),
+      workload: 15,
+      area: "Desenvolvimento de Softwares",
+      content: [
+        {
+          topic: "Lógica de programação",
+          subTopics: [
+            {id:0, item:"Variáveis"},
+            {id:1, item:"Tipos de variáveis"},
+            {id:2, item:"Estruturas de repetição"},
+            {id:3, item:"Funções"},
+            {id:4, item:"Objetos"},
+          ]
+        },
+        {
+          topic: "Análise de Dados",
+          subTopics: [
+            {id:0, item:"Numpy"},
+            {id:1, item:"Pandas"},
+            {id:2, item:"Keras"},
+          ]
+        }
+      ]
+    },
+    {
+      name:"Lean Manufacturing Basic",
+      providedBy: "CAE Treinamentos",
+      concludedIn: new Date(2021, 4, 1),
+      workload: 10,
+      area: "Engenharia",
+      content: [
+        {
+          topic: "Lean Thinking",
+          subTopics: [
+            {id:0, item:"Fundamentos Lean"},
+            {id:1, item:"Os 7 desperdícios do Lean"},
+            {id:2, item:"O oitavo desperdício"},
+            {id:3, item:"Fluxo de valor"},
+          ]
+        },
+        {
+          topic: "Ferramentas",
+          subTopics: [
+            {id:0, item:"Mapeamento do Fluxo de Valor (MFV)"},
+            {id:1, item:"Just-In-Time (JIT)"},
+            {id:2, item:"Kanban"},
+            {id:3, item:"Poka-Yoke"},
+          ]
+        },
+      ],
+    },
+    {
+      name:"Gestão de Ativos 4.0: Semana do Gestor de Manutenção",
+      providedBy: "Manutenção em Ação",
+      concludedIn: new Date(2021, 12, 17),
+      workload: 8,
+      area: "Engenharia",
+      content: [
+        {
+          topic: "Planejamento",
+          subTopics: [
+            {id:0, item:"Planos de inpeção"},
+            {id:1, item:"Planos de lubrificação"},
+          ]
+        },
+        {
+          topic: "Gerenciamento",
+          subTopics: [
+            {id:0, item:"Organização"},
+            {id:1, item:"Rastreabilidade"},
+            {id:2, item:"Padronização de procedimentos"},
+          ]
+        },
+        {
+          topic: "Engenharia",
+          subTopics: [
+            {id:0, item:"Análise de falhas"},
+            {id:1, item:"Análise dos Modos e Efeitos de Falha (FMEA)"},
+          ]
+        },
+      ],
+    },
+    {
+      name:"Acelerador de Carreira com Power BI",
+      providedBy: "Empowerdata",
+      concludedIn: new Date(2021, 10, 28),
+      workload: 8,
+      area: "Análise de Dados",
+      content: [
+        {
+          topic: "Introduçaõ",
+          subTopics: [
+            {id:0, item:"Estrutura do banco de dados"},
+            {id:1, item:"Gráficos"},
+          ]
+        },
+        {
+          topic: "Avançando",
+          subTopics: [
+            {id:0, item:"Dashboards"},
+            {id:1, item:"Funções DAX"},
+          ]
+        },
+      ],
+    },
+    {
+      name: "Lean Six Sigma: White Belt",
+      providedBy: "CAE Treinamentos",
+      concludedIn: new Date(2022, 4, 5),
+      workload: 10,
+      area: "Engenharia",
+      content: [
+        {
+          topic: "DMAIC - Define",
+          subTopics: [
+            {id:0, item:"Voz do Cliente (VOC)"},
+            {id:1, item:"Suppliers Inputs Process Outputs Costumers (SIPOC)"},
+          ]
+        },
+        {
+          topic: "DMAIC - Measure",
+          subTopics: [
+            {id:0, item:"Tipos de mapas de processos"},
+            {id:1, item:"Análise de valor agregado"},
+            {id:2, item:"Análises gráficas"},
+            {id:3, item:"Diagrama de Pareto"},
+          ]
+        },
+        {
+          topic: "DMAIC - Analyse",
+          subTopics: [
+            {id:0, item:"Análise de causa raiz"},
+            {id:1, item:"Matriz de Ishikawa"},
+            {id:2, item:"5 Porquês"},
+          ]
+        },
+        {
+          topic: "DMAIC - Improve",
+          subTopics: [
+            {id:0, item:"Estruturação do plano de melhorias"},
+            {id:1, item:"Ferramentas de priorização"},
+            {id:2, item:"Plano de ação 5W2H"},
+            {id:2, item:"Mapa de esforço e impacto"},
+          ]
+        },
+        {
+          topic: "DMAIC - Control",
+          subTopics: [
+            {id:0, item:"Controle e sustentabilidade de processos"},
+          ]
+        }
+      ]
+    },
+    {
+      name: "Lean Six Sigma: Yellow Belt",
+      providedBy: "Yokoten",
+      concludedIn: new Date(2022, 10, 8),
+      workload: 12,
+      area: "Engenharia",
+      content: [
+        {
+          topic: "Lean Thinking",
+          subTopics: [
+            {id:0, item:"Ciclo PDCA"},
+            {id:1, item:"Mapa de Fluxo de Valor (MFV)"},
+            {id:2, item:"Os 7 desperdicios do Lean"},
+            {id:3, item:"Diagrama de Espaguete"},
+            {id:4, item:"5 S"},
+            {id:4, item:"Kaizen"},
+            {id:4, item:"Melhoria Contínua"},
+          ]
+        },
+        {
+          topic: "Estatística",
+          subTopics: [
+            {id:0, item:"Curva de Gauss"},
+            {id:1, item:"Conceito de distribuição"},
+            {id:2, item:"Estatística Descritiva"},
+          ]
+        },
+        {
+          topic: "DMAIC - Define",
+          subTopics: [
+            {id:0, item:"Voz do Cliente (VOC)"},
+            {id:1, item:"Suppliers Inputs Process Outputs Costumers (SIPOC)"},
+          ]
+        },
+        {
+          topic: "DMAIC - Measure",
+          subTopics: [
+            {id:0, item:"Tipos de mapas de processos"},
+            {id:1, item:"Análise de valor agregado"},
+            {id:2, item:"Análises gráficas"},
+            {id:3, item:"Diagrama de Pareto"},
+          ]
+        },
+        {
+          topic: "DMAIC - Analyse",
+          subTopics: [
+            {id:0, item:"Análise de causa raiz"},
+            {id:1, item:"Matriz de Ishikawa"},
+            {id:2, item:"5 Porquês"},
+          ]
+        },
+        {
+          topic: "DMAIC - Improve",
+          subTopics: [
+            {id:0, item:"Estruturação do plano de melhorias"},
+            {id:1, item:"Ferramentas de priorização"},
+            {id:2, item:"Plano de ação 5W2H"},
+            {id:2, item:"Mapa de esforço e impacto"},
+          ]
+        },
+        {
+          topic: "DMAIC - Control",
+          subTopics: [
+            {id:0, item:"Controle e sustentabilidade de processos"},
+          ]
+        }
+      ]
+    },
+    {
+      name:"Gestão de Ativos 4.0: Maratona 4.0",
+      providedBy: "Manutenção em Ação",
+      concludedIn: new Date(2021, 9, 24),
+      workload: 20,
+      area: "Engenharia",
+      content: [
+        {
+          topic: "Planejamento",
+          subTopics: [
+            {id:0, item:"Planos de manutenções preventivas"},
+            {id:1, item:"Plano de 52 semanas"},
+          ]
+        },
+        {
+          topic: "Gerenciamento",
+          subTopics: [
+            {id:0, item:"Acompanhamento de métricas"},
+            {id:1, item:"Análise de desempenho"},
+            {id:2, item:"Padronização de procedimentos"},
+          ]
+        },
+        {
+          topic: "Engenharia",
+          subTopics: [
+            {id:0, item:"Análise de falhas"},
+            {id:1, item:"Análise dos Modos e Efeitos de Falha (FMEA)"},
+            {id:2, item:"Confiabilidade"},
+            {id:3, item:"Disponibilidade"},
+          ]
+        },
+      ],
+    },
+    {
+      name:"Mantendo o Estoque em Dia",
+      providedBy: "SEBRAE",
+      concludedIn: new Date(2023, 5, 26),
+      workload: 20,
+      area: "Engenharia",
+      content: [
+        {
+          topic: "Introdução",
+          subTopics: [
+            {id:0, item:"Estoque O que é? Por que é preciso controlá-lo?"},
+          ]
+        },
+        {
+          topic: "Organização",
+          subTopics: [
+            {id:0, item:"Organização física do estoque"},
+            {id:1, item:"Codificação do estoque"},
+          ]
+        },
+        {
+          topic: "Ferramentas",
+          subTopics: [
+            {id:0, item:"Giro de estoque"},
+            {id:1, item:"Técnicas de controle de estoque"},
+            {id:2, item:"Sistema ABC"},
+            {id:3, item:"Inventário"},
+          ]
+        },
+      ],
+    },
+  ]
+}
